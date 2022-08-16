c2739ccceb1fcda588395ba4f725b5d906db75f8
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2022-08-16 14:08:07 -0300

    chore: first commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/rodrigodosanjosoliveira/project
+
+go 1.19
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/lib/pq v1.10.6
+)
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/products", httpHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func httpHandler(w http.ResponseWriter, req *http.Request) {
+
+	response, err := getProducts()
+
+	if err != nil {
+		fmt.Fprintf(w, err.Error()+"\r\n")
+	} else {
+		enc := json.NewEncoder(w)
+		enc.SetIndent("", "  ")
+
+		if err := enc.Encode(response); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
